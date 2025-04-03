d516769f9ab46ac6fc3e7ea410c009592890ff41
Craig Matthew Espineli <126132263+cmespineli@users.noreply.github.com>
2025-04-03 10:55:52 -0500

    Buttons and Script Added

--- Assets/Scripts/GoToNextLevel.cs
+++ Assets/Scripts/GoToNextLevel.cs
@@ -5,10 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class GoToNextLevel : MonoBehaviour
 {
-    public void SceneTransition(string sceneName) 
-    { 
+    private const string LastSceneKey = "LastScene";
+
+    public void SceneTransition(string sceneName)
+    {
+        // Store the current scene before transitioning
+        PlayerPrefs.SetString(LastSceneKey, SceneManager.GetActiveScene().name);
+        PlayerPrefs.Save();
+
+        // Load the new scene
         SceneManager.LoadScene(sceneName);
-    } 
+    }
 
+    public void GoToLastScene()
+    {
+        // Retrieve the last scene and load it if it exists
+        string lastScene = PlayerPrefs.GetString(LastSceneKey, "");
 
+        if (!string.IsNullOrEmpty(lastScene))
+        {
+            SceneManager.LoadScene(lastScene);
+        }
+        else
+        {
+            Debug.LogWarning("No previous scene found!");
+        }
+    }
 }
